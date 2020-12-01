3d90de6e018f505dc73ff55772ceb8017f049acf
malek88888 <jmzomalek@gmail.com>
2020-12-01 22:23:41 +0200

    user info is rendered by userid

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const mainRouter = express.Router();
+const { addProduct, getproducts, deleteProduct, updateProduct, addStore, updateStore, getStores, deleteStore,
+  createItem, deleteItem, createOrder, getItems, getOrders } = require("../controllers/main-controller")
+const { register, getAllUsers, login ,getUserById} = require("../controllers/users_controller")
 
-mainRouter.get('/', (req, res) => {
-  res.json('HELLO WORLD');
-});
+mainRouter.post('/product', addProduct);
+mainRouter.get('/product', getproducts);
+mainRouter.delete('/product', deleteProduct);
+mainRouter.put('/product', updateProduct);
+
+mainRouter.post('/store', addStore);
+mainRouter.put('/store', updateStore);
+mainRouter.get('/store', getStores);
+mainRouter.delete('/store', deleteStore);
+
+mainRouter.post('/order', createOrder);
+mainRouter.get('/order', getOrders);
+mainRouter.delete('/order', deleteStore);
+
+mainRouter.post('/item', createItem);
+mainRouter.get('/item', getItems);
+mainRouter.delete('/item', deleteItem);
+
+mainRouter.post('/register', register);
+mainRouter.post('/login', login);
+mainRouter.get('/users', getAllUsers);
+mainRouter.get('/users/:user_id', getUserById);
+
 
 module.exports = mainRouter;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import axios from 'axios';
 
-// Class component
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <h1>HELLO WORLD</h1>
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+
+const App = (props) => {
+ 
+  const [userId, setUserId] = useState("")
+  const [Address, setAddress] = useState("")
+  const [Farstname, setFarstname] = useState("")
+  const [Lastname, setLastname] = useState("")
+  const [doB, doBset] = useState("")
+  const [email, setEmail] = useState("")
+  const [userPic, setUserPic] = useState("")
+  const [PhoneNumber, setPhoneNumber] = useState("")
+  const [Info, setInfo] = useState()
+  const getUser = async (infoArgumnt) => {
+    console.log(infoArgumnt)
+    axios
+      .get(`http://localhost:5000/users/${infoArgumnt}`)
+      .then(async (response) => {
+        console.log("response", response)
+        if (response.data.length === 0) {
+          alert("wrong user id")
+        }
+        setAddress(response.data[0].address)
+        setInfo(response.data[0])
+        setFarstname(response.data[0].first_name)
+        setLastname(response.data[0].last_name)
+        doBset(response.data[0].birthday)
+        setEmail(response.data[0].email)
+        setUserPic(response.data[0].image_profile)
+        setPhoneNumber(response.data[0].phone_number)
+      })
+      .catch((err) => {
+        console.log('RESULT: ', err);
+      });
+  };
+  return (
+    <Router>
+      <div className="app">
+        <p>APP</p>
+        <button onClick={() => getUser(userId)} > get user </button>
+        <input onChange={(e) => { setUserId(e.target.value) }} placeholder="user id" />
+        <div>
+          <p>{userPic}</p> <img src={userPic} alt="profile pic" className="pPic"></img>
+          <p>Address:{Address}</p>
+          <p>Farstname:{Farstname} Lastname:{Lastname}</p>
+          <p>birthday: {doB}</p>
+          <p>email:{email}</p>
+          <p> PhoneNumber:{PhoneNumber}</p>
+        </div>
       </div>
-    );
-  }
+    </Router>
+  )
 }
 
-/* 
-// functional component
-const App = () => {
-  return (
-    <div>
-      <h1>HELLO WORLD</h1>
-    </div>
-  );
-};
-export default App;
-*/
+
+export default App
