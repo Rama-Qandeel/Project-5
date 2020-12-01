84e0a129a09030afc23a8fbbc4cd931d2536d4ff
Rama-Qandeel <rama.qndeel@gmail.com>
2020-12-01 18:17:25 +0200

    remove space and consol log

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,44 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import axios from "axios";
 
-const Login=(props)=> {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-       
-    const handleChange = (event) => {
-        if (event.target.name === "email") {
-          setEmail(event.target.value);
-        }
-        if (event.target.name === "password") {
-          setPassword(event.target.value);
-        }
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (event) => {
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
     }
-    const handleSubmit = (event) => {
-        const data = {
-          email: email,
-          password: password,
-        };
-        axios
-        .post("http://localhost:5000/login", data)
-        .then((response) => {
-          if (response.data) {
-            //   console.log(response.data);
-              localStorage.setItem("token",response.data)
-            props.history.push("/home");
-          } 
-          
-        })
-        .catch((error) => {
-            if(error){
-                alert('Invalid User');
-              }
-        });
+    if (event.target.name === "password") {
+      setPassword(event.target.value);
+    }
+  };
+  const handleSubmit = (event) => {
+    const data = {
+      email: email,
+      password: password,
     };
-    
-    
-    return (
-            <div>
-            <div>
+    axios
+      .post("http://localhost:5000/login", data)
+      .then((response) => {
+        if (response.data) {
+          //   console.log(response.data);
+          localStorage.setItem("token", response.data);
+          props.history.push("/home");
+        }
+      })
+      .catch((error) => {
+        if (error) {
+          alert("Invalid User");
+        }
+      });
+  };
+
+  return (
+    <div>
+      <div>
         <input
           type="email"
           name="email"
@@ -60,9 +58,8 @@ const Login=(props)=> {
         />
       </div>
       <button onClick={handleSubmit}>Login</button>
-            </div>
-        )
-  
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
